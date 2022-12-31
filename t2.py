1dd7d786669622657d27c5ef73cad6435cccf908
Fabio <fabiosales_2l@hotmail.com>
2022-12-31 18:22:45 -0300

    na branch master, inseri outros prints da calculadora

--- t2.py
+++ t2.py
@@ -10,12 +10,16 @@ def produto(a,b):
 def divisao(a,b):
 	return a/b
 
-
-######################
+############################################
 
 def calculadora(a,b, func):
 	x = func(a,b)
 	return x
 
-
 print(calculadora(10,5, func = soma))
+
+############################################
+print(calculadora(10,5, func = soma))
+print(calculadora(10,5, func = subtracao))
+print(calculadora(10,5, func = produto))
+print(calculadora(10,5, func = divisao))
